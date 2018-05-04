56efe7bfeca79ca4a63e345cea83898dc0552ee0
ZhangXuemeng <Shirley@ZhangXuemengs-MacBook.local>
2018-05-04 18:36:25 -0400

    proxy issue

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,5 @@ module.exports = {
 	googleClientID: process.env.GOOGLE_CLIENT_ID,
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 	mongoURL: process.env.MONGO_URL,
-	cookieKey: process.env.COOKIE_KEY
+	cookieKey: process.env.COOKIE_KEY,
 }
